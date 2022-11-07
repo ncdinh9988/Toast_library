4409f2350207a1e832ca0718d0b75749b71d6749
dinh <ncdinh9988@gmail.com>
2022-11-07 13:55:43 +0700

    update2

--- mylibrary/src/main/java/com/fsg/mylibrary/ToasterMessage.java
+++ mylibrary/src/main/java/com/fsg/mylibrary/ToasterMessage.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ToasterMessage extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
